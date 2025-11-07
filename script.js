fdc195da2644c389e9b82fecbb0650bc85d2fb95
sanchezvalenciamarcos462-dev <sanchezvalenciamarcos462@gmail.com>
2025-11-07 00:07:46 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Menú responsivo
 const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".navbar a");
 
+// Abrir y cerrar menú
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  document.body.classList.toggle("menu-open"); // evita scroll
+});
+
+// Cerrar menú al hacer clic en cualquier enlace
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    document.body.classList.remove("menu-open");
+  });
 });
